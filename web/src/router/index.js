316c17bab3b165481dc9febdeea2654d29ddba71
hjy <571944067@qq.com>
2023-09-02 16:12:07 +0800

    前端游戏创建

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../Views/pk/PkIndexView'
+import RecordIndexView from '../Views/record/RecordIndexView'
+import RankListIndexView from '../Views/ranklist/RankListIndexView'
+import UserBotIndexView from '../Views/user/bot/UserBotIndexView'
+import NotFoundView from '../Views/error/NotFoundView'
 
 
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/"
+  },
 
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView
+  },
+
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView
+  },
+
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RankListIndexView
+  },
+
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView
+  },
+
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFoundView
+  },
+
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
+  }
 ]
 
 const router = createRouter({
